70f9d6c3bcb3d7362ecfdf2722ddd94818e4bf2d
SalasAlex25 <salasalexaldair@gmail.com>
2022-07-16 16:57:19 -0300

    Agregando  segundo parcial y ejercico2.5

---
+++ Problema2.5/Password.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema2._5
+{
+    class Password
+    {
+        private int longitud;
+        private string valor;
+
+        public Password()
+        {
+            longitud = 8;
+            valor=String.Empty;
+        }
+
+        public Password(string valor){
+            this.valor = valor;
+        }
+
+        public string Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public int Longitud
+        {
+            get { return longitud; }
+        }
+
+        public bool EsFuerte()
+        {
+            bool esFuerte=false;
+            if(valor=="")
+
+            return esFuerte;
+        }
+        
+
+    }
+}
